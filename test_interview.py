b3476b0e51113036ea3554f6efdada83806f9e82
Master-cp <aitmansour.med@gmail.com>
2025-09-02 23:52:19 -0400

    Update test_interview.py

--- test_interview.py
+++ test_interview.py
@@ -378,7 +378,9 @@ def speech_to_text():
                 )
                 # Nettoyer le fichier temporaire
                 os.unlink(tmp_path)
-
+                st.session_state.audio_data = None
+                st.experimental_rerun()
+              
                 # Effacer l'audio du widget pour permettre un nouvel enregistrement
                 st.session_state.pop("audio_input", None)
             return transcription.text or "Aucune parole détectée. Veuillez réessayer."
